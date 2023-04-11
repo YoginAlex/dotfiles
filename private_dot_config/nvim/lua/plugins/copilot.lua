aca1cb1d8bb36161bd92cc57534519a6ea55cbff
Alexey Zakharenko <yogin.alex@gmail.com>
2023-04-11 16:34:43 +0400

    Update .config/nvim/lua/plugins/copilot.lua

--- private_dot_config/nvim/lua/plugins/copilot.lua
+++ private_dot_config/nvim/lua/plugins/copilot.lua
@@ -1,8 +1,11 @@
 return {
   "github/copilot.vim",
+  cmd = "Copilot",
+  build = ":Copilot auth",
   init = function()
     vim.g.copilot_no_tab_map = true
     vim.g.copilot_assume_mapped = true
     vim.api.nvim_set_keymap("i", "<C-f>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
   end,
 }
+
