71322845c5a5c5026c87453903b5ad585d03c1c8
Alexey Zakharenko <yogin.alex@gmail.com>
2023-04-11 16:37:43 +0400

    Update .config/nvim/lua/plugins/telescope.lua

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -87,11 +87,11 @@ return {
       }
     end,
     config = function(_, opts)
+      require("telescope").setup(opts)
+
       require("telescope").load_extension("fzf")
       require("telescope").load_extension("neoclip")
       require("telescope").load_extension("file_browser")
-
-      require("telescope").setup(opts)
     end,
   }
 }
