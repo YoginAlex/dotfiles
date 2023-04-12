06dfae960d7d684c454765d295e0b578b6c0e870
Alexey Zakharenko <yogin.alex@gmail.com>
2023-04-12 13:22:35 +0400

    Update .config/nvim/lua/plugins/neoclip.lua

--- private_dot_config/nvim/lua/plugins/neoclip.lua
+++ private_dot_config/nvim/lua/plugins/neoclip.lua
@@ -1,7 +1,10 @@
 return {
   "AckslD/nvim-neoclip.lua",
   dependencies = {
-    { "kkharji/sqlite.lua",           module = "sqlite" },
+    {
+      "kkharji/sqlite.lua",
+      module = "sqlite"
+    },
     { "nvim-telescope/telescope.nvim" }
   },
   config = function()
