b38a7d789a8f7d30447331d3037695e3a7bca1a7
Alexey Zakharenko <yogin.alex@gmail.com>
2023-04-12 19:51:54 +0400

    Update .config/nvim/lua/plugins/telescope.lua

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -9,25 +9,6 @@ return {
     "nvim-telescope/telescope.nvim",
     version = false,
     dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      {
-        "<leader>?",
-        require("telescope.builtin").oldfiles,
-        desc = "[?] Find recently opened files"
-      },
-      { "<leader><space>", require("telescope.builtin").buffers,     desc = "[ ] Find existing buffers" },
-      { "<leader>sf",      require("telescope.builtin").find_files,  desc = "[S]earch [F]iles" },
-      { "<leader>sh",      require("telescope.builtin").help_tags,   desc = "[S]earch [H]elp" },
-      { "<leader>sw",      require("telescope.builtin").grep_string, desc = "[S]earch current [W]ord" },
-      { "<leader>sg",      require("telescope.builtin").live_grep,   desc = "[S]earch by [G]rep" },
-      { "<leader>sd",      require("telescope.builtin").diagnostics, desc = "[S]earch [D]iagnostics" },
-      -- { "<leader>su",      require("telescope").extensions.file_browser.file_browser, desc = "[S]earch c[u]rrent" },
-      {
-        "<leader>/",
-        require('telescope').extensions.neoclip.default,
-        desc = "[/] Search clipboard history"
-      },
-    },
     opts = function()
       -- Clone the default Telescope configuration
       local vimgrep_arguments = { unpack(require("telescope.config").values.vimgrep_arguments) }
@@ -77,20 +58,20 @@ return {
           },
         },
         extensions = {
-          -- file_browser = {
-          --   layout_strategy = "vertical",
-          --   path = "%:p:h",
-          --   mappings = {
-          --     i = {
-          --       ["<C-t>"] = actions.select_tab,
-          --       ["<C-z>"] = fb_actions.change_cwd,
-          --     },
-          --     n = {
-          --       t = actions.select_tab,
-          --       z = fb_actions.change_cwd,
-          --     },
-          --   },
-          -- }
+          file_browser = {
+            layout_strategy = "vertical",
+            path = "%:p:h",
+            mappings = {
+              i = {
+                ["<C-t>"] = actions.select_tab,
+                ["<C-z>"] = fb_actions.change_cwd,
+              },
+              n = {
+                t = actions.select_tab,
+                z = fb_actions.change_cwd,
+              },
+            },
+          }
         }
       }
     end,
@@ -101,5 +82,24 @@ return {
       require("telescope").load_extension("neoclip")
       -- require("telescope").load_extension("file_browser")
     end,
+    keys = {
+      {
+        "<leader>?",
+        require("telescope.builtin").oldfiles,
+        desc = "[?] Find recently opened files"
+      },
+      { "<leader><space>", require("telescope.builtin").buffers,                      desc = "[ ] Find existing buffers" },
+      { "<leader>sf",      require("telescope.builtin").find_files,                   desc = "[S]earch [F]iles" },
+      { "<leader>sh",      require("telescope.builtin").help_tags,                    desc = "[S]earch [H]elp" },
+      { "<leader>sw",      require("telescope.builtin").grep_string,                  desc = "[S]earch current [W]ord" },
+      { "<leader>sg",      require("telescope.builtin").live_grep,                    desc = "[S]earch by [G]rep" },
+      { "<leader>sd",      require("telescope.builtin").diagnostics,                  desc = "[S]earch [D]iagnostics" },
+      { "<leader>su",      require("telescope").extensions.file_browser.file_browser, desc = "[S]earch c[u]rrent" },
+      {
+        "<leader>/",
+        require('telescope').extensions.neoclip.default,
+        desc = "[/] Search clipboard history"
+      },
+    },
   }
 }
