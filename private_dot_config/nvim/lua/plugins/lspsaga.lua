852650122f9b86db0f0cc4dcd786450650e02585
Alexey Zakharenko <yogin.alex@gmail.com>
2023-04-12 21:11:29 +0400

    Update .config/nvim/lua/plugins/lspsaga.lua

--- private_dot_config/nvim/lua/plugins/lspsaga.lua
+++ private_dot_config/nvim/lua/plugins/lspsaga.lua
@@ -2,14 +2,14 @@ return {
   "glepnir/lspsaga.nvim",
   event = "LspAttach",
   dependencies = {
-    {"nvim-tree/nvim-web-devicons"},
+    { "nvim-tree/nvim-web-devicons" },
     --Please make sure you install markdown and markdown_inline parser
-    {"nvim-treesitter/nvim-treesitter"}
+    { "nvim-treesitter/nvim-treesitter" }
   },
   opts = {
     lightbulb = {
       enabled = false,
-      sign = false,
+      sign = true,
       sign_priority = 20,
       virtual_text = true,
     },
@@ -40,27 +40,27 @@ return {
     -- Lsp finder find the symbol definition implement reference
     -- when you use action in finder like open vsplit then you can
     -- use <C-t> to jump back
-    { "gh", "<cmd>Lspsaga lsp_finder<CR>"},
+    { "gh",         "<cmd>Lspsaga lsp_finder<CR>" },
     -- Code action
     { "<leader>ca", "<cmd>Lspsaga code_action<CR>" },
-    { "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>", mode = { "v" }},
+    { "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>", mode = { "v" } },
     -- Rename
-    { "gr", "<cmd>Lspsaga rename<CR>", silent = true, remap = false },
+    { "gr",         "<cmd>Lspsaga rename<CR>",                 silent = true, remap = false },
     -- Peek definition
     -- You can edit the file containing the definition in the floating window
     -- It also supports open/vsplit/etc operations, do refer to "definition_action_keys"
     -- It also supports tagstack
     -- Use <C-t> to jump back
-    { "gp", "<cmd>Lspsaga peek_definition<CR>" },
+    { "gp",         "<cmd>Lspsaga peek_definition<CR>" },
     -- Go to definition
-    { "gd", "<cmd>Lspsaga goto_definition<CR>" },
+    { "gd",         "<cmd>Lspsaga goto_definition<CR>" },
     -- Show line diagnostics
-    { "<leader>e", "<cmd>Lspsaga show_line_diagnostics<CR>" },
+    { "<leader>e",  "<cmd>Lspsaga show_line_diagnostics<CR>" },
     -- Show cursor diagnostic
-    { "<leader>e", "<cmd>Lspsaga show_cursor_diagnostics<CR>" },
+    { "<leader>e",  "<cmd>Lspsaga show_cursor_diagnostics<CR>" },
     -- Diagnsotic jump
-    { "[E", "<cmd>Lspsaga diagnostic_jump_next<CR>" },
-    { "]E", "<cmd>Lspsaga diagnostic_jump_prev<CR>" },
+    { "[E",         "<cmd>Lspsaga diagnostic_jump_next<CR>" },
+    { "]E",         "<cmd>Lspsaga diagnostic_jump_prev<CR>" },
     -- Diagnostic jump with filters such as only jumping to an error
     { "[e",
       function()
@@ -72,12 +72,12 @@ return {
         require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
       end
     },
-    { "<leader>q", "<cmd>Lspsaga show_buf_diagnostics<CR>" },
+    { "<leader>q",  "<cmd>Lspsaga show_buf_diagnostics<CR>" },
     { "<leader>sw", "<cmd>Lspsaga show_workspace_diagnostics<CR>" },
-    { "<leader>o", "<cmd>Lspsaga outline<CR>" },
-    { "K", "<cmd>Lspsaga hover_doc<CR>", silent = true },
+    { "<leader>o",  "<cmd>Lspsaga outline<CR>" },
+    { "K",          "<cmd>Lspsaga hover_doc<CR>",                 silent = true },
   },
   config = function(_, opts)
-      require("lspsaga").setup(opts)
+    require("lspsaga").setup(opts)
   end
 }
