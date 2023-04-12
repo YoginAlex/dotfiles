e7a3c4d61cc699b8d74ee0c59c08b42413037eaa
Alexey Zakharenko <yogin.alex@gmail.com>
2023-04-12 20:59:58 +0400

    Update .config/nvim/lua/plugins/init.lua

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -1,10 +1,11 @@
 return {
-  {"nvim-tree/nvim-web-devicons",lazy = true},
+  { "tpope/vim-fugitive" },
+  { "nvim-tree/nvim-web-devicons", lazy = true },
   {
     "windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup {} end
   },
-  {"windwp/nvim-ts-autotag"},
+  { "windwp/nvim-ts-autotag" },
   {
     "max397574/better-escape.nvim",
     config = function() require("better_escape").setup({ mapping = { "jj", "jk" } }) end
@@ -12,7 +13,7 @@ return {
   {
     "numToStr/Comment.nvim",
     config = function()
-        require("Comment").setup()
+      require("Comment").setup()
     end
   }
 }
