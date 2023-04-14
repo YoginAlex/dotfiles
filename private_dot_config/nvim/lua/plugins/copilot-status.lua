896bae5339e82146027b003d67da5e91e7f981ee
Alexey Zakharenko <yogin.alex@gmail.com>
2023-04-14 15:29:48 +0400

    Update .config/nvim/lua/plugins/copilot-status.lua

--- private_dot_config/nvim/lua/plugins/copilot-status.lua
+++ private_dot_config/nvim/lua/plugins/copilot-status.lua
@@ -2,7 +2,6 @@ return {
   "jonahgoldwastaken/copilot-status.nvim",
   dependencies = { "zbirenbaum/copilot.lua" },
   lazy = true,
-  event = "BufReadPost",
   config = function()
     require("copilot_status").setup()
   end,
