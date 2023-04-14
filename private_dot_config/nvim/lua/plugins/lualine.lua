307798e921362d490ef10367e7b07cee9ea51e02
Alexey Zakharenko <yogin.alex@gmail.com>
2023-04-14 15:54:08 +0400

    Update .config/nvim/lua/plugins/lualine.lua

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -2,8 +2,6 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
   opts = function()
-    local copilot_status = require("copilot_status").status_string
-
     return {
       options = {
         icons_enabled = true,
@@ -16,7 +14,7 @@ return {
         lualine_b = { 'diff', 'branch' },
         lualine_c = { { 'filename', path = 0, file_status = true } },
         lualine_x = { 'diagnostics' },
-        lualine_y = { 'filetype', copilot_status },
+        lualine_y = { 'filetype' },
         lualine_z = { 'location', 'progress' },
       },
     }
