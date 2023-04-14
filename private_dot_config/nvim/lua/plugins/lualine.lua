53fde89166f3f751a0eb81f5a65779d602185b2b
Alexey Zakharenko <yogin.alex@gmail.com>
2023-04-14 14:53:13 +0400

    Update .config/nvim/lua/plugins/lualine.lua

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -1,23 +1,26 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = 'auto',
-      component_separators = { left = '', right = '' },
-      section_separators = { left = '', right = '' },
-      shorting_target = 40,
-    },
-    sections = {
-      lualine_b = { 'diff', 'branch' },
-      lualine_c = { { 'filename', path = 0, file_status = true } },
-      lualine_x = { 'diagnostics' },
-      lualine_y = { 'filetype' },
-      lualine_z = { 'location', 'progress' },
-    },
-  },
-  ---@param opts TSConfig
+  opts = function()
+    local copilot_status = require("copilot_status").status_string
+
+    return {
+      options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        shorting_target = 40,
+      },
+      sections = {
+        lualine_b = { 'diff', 'branch' },
+        lualine_c = { { 'filename', path = 0, file_status = true } },
+        lualine_x = { 'diagnostics' },
+        lualine_y = { 'filetype', copilot_status },
+        lualine_z = { 'location', 'progress' },
+      },
+    }
+  end,
   config = function(_, opts)
     require("lualine").setup(opts)
   end,
