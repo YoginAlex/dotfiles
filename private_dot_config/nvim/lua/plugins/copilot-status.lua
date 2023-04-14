e2f1150275a237ccd56adbc5a24f2f4edb8b4c13
Alexey Zakharenko <yogin.alex@gmail.com>
2023-04-14 15:52:24 +0400

    Remove .config/nvim/lua/plugins/copilot-status.lua

--- private_dot_config/nvim/lua/plugins/copilot-status.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "jonahgoldwastaken/copilot-status.nvim",
-  dependencies = { "zbirenbaum/copilot.lua" },
-  lazy = false,
-  config = function()
-    require("copilot_status").setup()
-  end,
-}
