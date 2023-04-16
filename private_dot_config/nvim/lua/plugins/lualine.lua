4f0e9fa72a8cdb15f992201da6a420fc75a560ba
Alexey Zakharenko <yogin.alex@gmail.com>
2023-04-16 21:52:14 +0400

    Add .config/nvim/lua/plugins/battery.lua Update .config/nvim/lua/plugins/lualine.lua

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -2,20 +2,26 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
   opts = function()
+    local nvimbattery = {
+      function()
+        return require("battery").get_status_line()
+      end,
+      color = { fg = colors.violet, bg = colors.bg },
+    }
     return {
       options = {
         icons_enabled = true,
-        theme = 'auto',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         shorting_target = 40,
       },
       sections = {
-        lualine_b = { 'diff', 'branch' },
-        lualine_c = { { 'filename', path = 0, file_status = true } },
-        lualine_x = { 'diagnostics' },
-        lualine_y = { 'filetype' },
-        lualine_z = { 'location', 'progress' },
+        lualine_b = { "diff", "branch" },
+        lualine_c = { { "filename", path = 0, file_status = true } },
+        lualine_x = { "diagnostics" },
+        lualine_y = { "filetype" },
+        lualine_z = { "location", "progress", nvimbattery },
       },
     }
   end,
