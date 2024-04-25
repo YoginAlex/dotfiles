46b0d25a46a35ba925de9ccd3e951494baf83977
Alexey Zakharenko <yogin.alex@gmail.com>
2024-04-25 20:15:40 +0200

    Remove .config/nvim/lua/plugins/battery.lua Remove .config/nvim/lua/plugins/cmp.lua Remove .config/nvim/lua/plugins/colorizer.lua Remove .config/nvim/lua/plugins/doom-one.lua

--- private_dot_config/nvim/lua/plugins/colorizer.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "NvChad/nvim-colorizer.lua",
-  init = function ()
-    require("colorizer").setup()
-  end,
-}
--- private_dot_config/nvim/lua/plugins/doom-one.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'romgrk/doom-one.vim',
-  config = function(_, opts)
-    -- load the colorscheme here
-    -- vim.cmd("colorscheme doom-one")
-  end,
-}
