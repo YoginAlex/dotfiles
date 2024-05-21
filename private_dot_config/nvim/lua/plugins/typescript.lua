a95a24dcad9d5bdf1b3ee434ccdf2fd528629adf
Alexey Zakharenko <yogin.alex@gmail.com>
2024-05-21 11:17:17 +0200

    Update .config/nvim/lua/plugins/typescript.lua

--- private_dot_config/nvim/lua/plugins/typescript.lua
+++ private_dot_config/nvim/lua/plugins/typescript.lua
@@ -44,51 +44,34 @@ return {
       "typescript.tsx",
     },
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    config = function(opts)
-      require("lazyvim.util.lsp").on_attach(function(client, bufnr)
-        if client.name == "typescript-tools" then
-          keymap(
-            "n",
-            "<leader>la",
-            "<cmd>TSToolsAddMissingImports<cr>",
-            { buffer = bufnr, desc = "Add Missing Imports" }
-          )
-          keymap(
-            "n",
-            "<leader>lo",
-            "<cmd>TSToolsOrganizeImports<cr>",
-            { buffer = bufnr, desc = "Organize Imports" }
-          )
-          keymap(
-            "n",
-            "<leader>lO",
-            "<cmd>TSToolsSortImports<cr>",
-            { buffer = bufnr, desc = "Sort Imports" }
-          )
-          keymap(
-            "n",
-            "<leader>lu",
-            "<cmd>TSToolsRemoveUnused<cr>",
-            { buffer = bufnr, desc = "Removed Unused" }
-          )
-          keymap(
-            "n",
-            "<leader>lr",
-            "<cmd>TSToolsRemoveUnusedImports<cr>",
-            { buffer = bufnr, desc = "Removed Unused Imports" }
-          )
-          keymap(
-            "n",
-            "<leader>ld",
-            "<cmd>TSToolsGoToSourceDefinition<cr>",
-            { buffer = bufnr, desc = "Go To Source Definition" }
-          )
-          keymap("n", "<leader>lf", "<cmd>TSToolsFixAll<cr>", { buffer = bufnr, desc = "Fix All" })
-          require("twoslash-queries").attach(client, bufnr)
-        end
-      end)
-      require("typescript-tools").setup(opts)
-    end,
+    keys = {
+      { "<leader>la", "<cmd>TSToolsOrganizeImports<cr>", desc = "Add Missing Imports" },
+      { "<leader>lo", "<cmd>TSToolsOrganizeImports<cr>", desc = "Organize Imports" },
+      { "<leader>lO", "<cmd>TSToolsSortImports<cr>", desc = "Sort Imports" },
+      { "<leader>lu", "<cmd>TSToolsRemoveUnused<cr>", desc = "Removed Unused" },
+      { "<leader>lr", "<cmd>TSToolsRemoveUnusedImports<cr>", desc = "Removed Unused Imports" },
+      { "<leader>ld", "<cmd>TSToolsGoToSourceDefinition<cr>", desc = "Go To Source Definition" },
+      { "<leader>lf", "<cmd>TSToolsFixAll<cr>", desc = "Fix All" },
+    },
+    opts = {
+      settings = {
+        expose_as_code_action = "all",
+        jsx_close_tag = {
+          enable = true,
+          filetypes = { "javascriptreact", "typescriptreact" },
+        },
+        tsserver_file_preferences = {
+          includeInlayParameterNameHints = "all",
+          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+      },
+    },
   },
   {
     "neovim/nvim-lspconfig",
